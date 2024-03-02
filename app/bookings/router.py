0b5ba522c52ceb65b338a12e62744674cfdbeb5d
Object-626 <shtokov6161@gmail.com>
2024-03-03 01:42:25 +0400

    закончил upgrade endpoint

---
+++ app/bookings/router.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from app.bookings.dao import BookingDAO
+from app.bookings.schemas import SBooking
+
+
+router = APIRouter(
+    prefix="/bookings",
+    tags=["Бронирование"]
+)
+
+
+@router.get("")
+async def get_bookings() -> list[SBooking]:
+    return await BookingDAO.find_all()
+
+
+
